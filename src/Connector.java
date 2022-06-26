48291a486d48e34c9ee765b920ba8e434e578977
David Pazouki <david.pazouki@accenture.com>
2022-06-26 23:59:07 +0200

    I changed print function

--- src/Connector.java
+++ src/Connector.java
@@ -1,6 +1,6 @@
 public class MainClass {
     public static void main(String[] args){
         system.out.println("different line of code")
-        system.out.println("different line of code this line I just added")
+        system.out.println("someone changes the txt here")
     }
 }
